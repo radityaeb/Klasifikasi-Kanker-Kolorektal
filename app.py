a89c804f444091ec31447fd5d352771497ca1d08
radityaeb <radityaeb@gmail.com>
2026-05-27 22:14:26 +0700

    Fix prediction logic for cancer classification

--- app.py
+++ app.py
@@ -37,10 +37,10 @@ def classify_image(image):
     probabilitas_kanker = float(predictions[0][0])
 
     if probabilitas_kanker > 0.5:
-        kelas_prediksi = 'kanker'
+        kelas_prediksi = 'normal'
         skor_keyakinan = probabilitas_kanker  
     else:
-        kelas_prediksi = 'normal'
+        kelas_prediksi = 'kanker'
         skor_keyakinan = 1.0 - probabilitas_kanker
         
     # max_index = np.argmax(predictions[0])
